b57c888bf37e26b12d0d72bd7a7ddbc561842423
Jefferson Neves <jefferson.rpn@gmail.com>
2019-04-25 15:58:17 -0300

    Envia resposta simples

--- server.js
+++ server.js
@@ -8,3 +8,21 @@ client.setWebhook(`https://voz-ativa-bot.herokuapp.com/${WEBHOOK_TOKEN}`);
 
 const app = express();
 app.use(`/${WEBHOOK_TOKEN}`, require('./routes/message'));
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send("ok");
+})
+
+app.post('/', (req, res, next) => {
+  console.log(req.body.message);
+    client
+        .sendMessage(req.body.message.chat.id, 'I\'m a bot, so what?')
+        .promise()
+        // replying succeeded, so we can send a 200 response to Telegram
+        // the actual payload does not matter
+        .then(() => res.json({ ok: true }))
+        // something above failed, we will use express' default error handling
+        .catch(next);
+});
