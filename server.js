75e904b71e89051c74d17ef80d94493063b833de
Jefferson Neves <jefferson.rpn@gmail.com>
2019-04-25 17:04:18 -0300

    Remove promise

--- server.js
+++ server.js
@@ -18,14 +18,7 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res, next) => {
   console.log(req.body.message);
-    client
-        .sendMessage(req.body.message.chat.id, 'I\'m a bot, so what?')
-        .promise()
-        // replying succeeded, so we can send a 200 response to Telegram
-        // the actual payload does not matter
-        .then(() => res.json({ ok: true }))
-        // something above failed, we will use express' default error handling
-        .catch(next);
+  client.sendMessage(req.body.message.chat.id, 'I\'m a bot, so what?');
 });
 
 app.use((req, res, next) => {
