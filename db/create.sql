662588cca0bb34459aa409b72ba6e1762afc9845
Jefferson Neves <jefferson.rpn@gmail.com>
2019-04-26 10:09:52 -0300

    Cria banco de dados

---
+++ db/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS "admins" (
+    "id" SERIAL, 
+    "username" VARCHAR(50),
+    "chatid" VARCHAR(50),
+PRIMARY KEY ("id"));
+
+CREATE TABLE IF NOT EXISTS "logs" (
+    "id" SERIAL, 
+    "timestamp" TIMESTAMP,
+    "log" TEXT,
+PRIMARY KEY ("id"));
