805b85021cfb7781ee6b94f322261f2790dbba70
Jefferson Neves <jefferson.rpn@gmail.com>
2021-03-17 17:57:13 -0300

    Altera forma de conexão com o bd

--- database.js
+++ database.js
@@ -1,30 +1,50 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
+const { Client } = require('pg')
+
+const client = new Client({
   user: process.env.DATABASE_USER || 'postgres',
   host: process.env.DATABASE_HOST || 'localhost',
   database: process.env.DATABASE_NAME || 'vozativabot',
   password: process.env.DATABASE_PASSWORD || 'secret',
-  port: 5432
-})
+  port: 5432,
+  connectionTimeoutMillis: 10000,
+  ssl: true
+});
 
 const getAdmins = () => {
   return new Promise((resolve, reject) => {
-    pool.query('SELECT * FROM admins', (error, results) => {
-      if (error) {
-        reject(error);
+    client.connect(err => {
+      if (err) {
+        reject(err);
+      } else {
+        client.query('SELECT * FROM admins', (error, results) => {
+          if (error) {
+            client.end()
+            reject(error);
+          } else {
+            client.end()
+            resolve(results);
+          }
+        })
       }
-      resolve(results);
     })
   })
 }
 
 const saveLog = (log) => {
   return new Promise((resolve, reject) => {
-    pool.query('INSERT INTO logs (log) VALUES ($1)', [log], (error, results) => {
-      if (error) {
-        reject(error);
+    client.connect(err => {
+      if (err) {
+        reject("Erro na conexão com o bd");
+      } else {
+        client.query('INSERT INTO logs (log) VALUES ($1)', [log], (error, results) => {
+          if (error) {
+            client.end()
+            reject(error);
+          }
+          client.end()
+          resolve(results);
+        })
       }
-      resolve(results);
     })
   })
 }
